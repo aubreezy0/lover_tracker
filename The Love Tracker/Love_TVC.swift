7ed98b6530c02c0612740321ab31631589755746
Aubree Lawrence <aubreezy0@gmail.com>
2019-10-15 14:55:54 -0400

    Add functionality to fetch and create Love objects Add function getLoves() to fetch objects from Core Data Add function loveTapped button to add object to CoreData

--- The Love Tracker/Love_TVC.swift
+++ The Love Tracker/Love_TVC.swift
@@ -10,29 +10,36 @@ import UIKit
 
 class Love_TVC: UITableViewController {
 
+    // hold Love objects
     var allLoves : [Love] = []
     
     override func viewWillAppear(_ animated: Bool) {
         getLoves()
     }
 
+    // fetch love objects from core data
     func getLoves() {
         if let context = (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer.viewContext {
-            
-            tableView.reloadData()
+            if let loves = try? context.fetch(Love.fetchRequest()) {
+                if let theLove = loves as? [Love] {
+                    allLoves = theLove
+                    tableView.reloadData()
+                }
+            }
         }
     }
     
+    // add love objects to coredata on tap
     @IBAction func loveTapped(_ sender: Any) {
-        // ADD object
-        
         if let context = (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer.viewContext {
-
+            let love = Love(context:context)
+            love.name = "❤️ You are Loved! ❤️"
         (UIApplication.shared.delegate as? AppDelegate)?.saveContext()
         getLoves()
         }
     }
     
+    // delete love objects from core data, sad face
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let selectedLove = allLoves[indexPath.row]
         if let context = (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer.viewContext {
@@ -43,19 +50,14 @@ class Love_TVC: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-
         return allLoves.count
     }
 
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell()
-
         let selectedLove = allLoves[indexPath.row]
         cell.textLabel?.text = selectedLove.name
-
         return cell
     }
-    
-
 }
