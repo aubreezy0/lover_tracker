4dc88b5b37d252a59cc6feed920802358d5065a9
Aubree Lawrence <aubreezy0@gmail.com>
2019-10-15 13:17:28 -0400

    Initial Commit

---
+++ The Love Tracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  The Love Tracker
+//
+//  Created by Aubree Lawrence on 10/15/19.
+//  Copyright © 2019 Aubree Lawrence. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
